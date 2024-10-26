7eee1fe7d41c31454b66e0bcd39ae12dc2d55814
nonya123456 <nonya579@gmail.com>
2024-10-26 16:01:33 +0700

    add placing

--- internal/service/service.go
+++ internal/service/service.go
@@ -65,6 +65,35 @@ func (s *Service) Place(messageID string, userID string, i int) (model.Match, bo
 		return model.Match{}, false, nil
 	}
 
+	if match.RoundNumber%2 == 1 && match.Host != userID {
+		return model.Match{}, false, nil
+	}
+
+	if match.RoundNumber%2 == 0 && *match.Guest != userID {
+		return model.Match{}, false, nil
+	}
+
+	board, err := match.Board()
+	if err != nil {
+		return model.Match{}, false, err
+	}
+
+	placed := false
+	for j := 0; j < 6; j++ {
+		if board[j][i-1] != 0 {
+			continue
+		}
+
+		board[j][i-1] = 2 - (match.RoundNumber % 2)
+		placed = true
+		break
+	}
+
+	if !placed {
+		return model.Match{}, false, nil
+	}
+
+	match.BoardString = util.BoardString(board)
 	match.RoundNumber += 1
 
 	match, err = s.repo.SaveMatch(match)
